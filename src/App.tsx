16b63939cc5966ba8b7dd33b96ab00390ee15a8f
aldrazen <drazgamings@gmail.com>
2025-10-05 07:43:33 +0800

    tailwind and shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@/components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <div className="flex items-center justify-center">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,18 +19,21 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Button
+          variant="outline"
+          className="!bg-rose-400 text-white hover:bg-blue-600 hover:text-white hover:shadow-lg mb-3 hover:!border-rose-300"
+          size="lg"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
 export default App
